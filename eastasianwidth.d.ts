af9ddb8f35a8936a907c772da4bc6a83f81e275c
skytomo <tomo1kawa.fullerene@gmail.com>
2024-04-21 01:54:07 +0900

    Create eastasianwidth.d.ts

---
+++ eastasianwidth.d.ts
@@ -0,0 +1,4 @@
+export function eastAsianWidth(character: string): "F" | "H" | "W" | "Na" | "A" | "N";
+export function characterLength(character: string): 1 | 2;
+export function length(string: string): number;
+export function slice(text: string, start: number | null | undefined, end: number | undefined): string;
